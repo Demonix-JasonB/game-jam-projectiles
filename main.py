09dec942cbc26319f6367b406522d324fcd08e9d
Demonix-JasonB <69871102+Demonix-JasonB@users.noreply.github.com>
2020-08-26 10:02:14 -0400

    Update main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
-#Setup the game
+# Setup the game
 info.set_score(0)
 info.set_life(3)
-scene.set_background_image(img("""
-    f 5 f f f f f f f f f f f f f f
-    f f f 5 f f f f f f 5 f f f f f
-    f f f f f f f 5 f f f f f 5 f f
-    f f 5 f f f f f f f f f f f f f
-    f f f f f f f f f f f f f f f f
-    f f f f f f f f f f f f 5 f f f
-    f f f 5 f f 5 5 f f f f f f f f
-    f f f f f f f f f f f f f f f f
-    f f f f f f f 5 f 5 f f f f f f
-    f f 5 f f f f f f f f f f f f f
-    f f f f f f f 5 f f f f f 5 f f
-    f f f f f f f f f f f f f f f f
-    f f f f f f f f f f f f f f f f
-    f f f f 5 f f f f f f f f f f f
-    f f 5 f f f f f 5 f f f f 5 f f
-    f f f f f f f 5 f f f f f f f f
-"""))
-tiles.set_tilemap(tilemap("""level"""))
-#Setup player
+tiles.set_tilemap(tilemap("""level_0"""))
+# Setup player
 Spaceship = sprites.create(img("""
-    . . . . . . . 2 2 . . . . . . .
-    . . . . . . 2 2 2 2 . . . . . .
-    . . . . . 2 2 1 1 2 2 . . . . .
-    2 . . . 2 2 1 1 1 1 2 2 . . . 2
-    2 . . . 2 1 1 1 1 1 1 2 . . . 2
-    1 . . . 2 2 1 1 1 1 2 2 . . . 1
-    1 . . f 2 2 2 2 2 2 2 2 f . . 1
-    1 . . 2 f 2 2 2 2 2 2 f 2 . . 1
-    2 . . 2 2 f 2 f f 2 f 2 2 . . 2
-    2 . 2 2 2 2 f 2 2 f 2 2 2 2 . 2
-    2 2 2 2 2 f 2 f f 2 f 2 2 2 2 2
-    2 . 2 2 2 f 2 f f 2 f 2 2 2 . 2
-    2 2 2 2 2 2 f 2 2 f 2 2 2 2 2 2
-    2 . 1 1 2 f 2 f f 2 f 2 1 1 . 2
-    2 . 1 1 1 2 2 2 2 2 2 1 1 1 . 2
-    . . . 1 1 1 1 2 2 1 1 1 1 . . .
-"""))
-#Configure Player Controls
+        . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 1 1 2 2 . . . . . 
+            2 . . . 2 2 1 1 1 1 2 2 . . . 2 
+            2 . . . 2 1 1 1 1 1 1 2 . . . 2 
+            1 . . . 2 2 1 1 1 1 2 2 . . . 1 
+            1 . . f 2 2 2 2 2 2 2 2 f . . 1 
+            1 . . 2 f 2 2 2 2 2 2 f 2 . . 1 
+            2 . . 2 2 f 2 f f 2 f 2 2 . . 2 
+            2 . 2 2 2 2 f 2 2 f 2 2 2 2 . 2 
+            2 2 2 2 2 f 2 f f 2 f 2 2 2 2 2 
+            2 . 2 2 2 f 2 f f 2 f 2 2 2 . 2 
+            2 2 2 2 2 2 f 2 2 f 2 2 2 2 2 2 
+            2 . 1 1 2 f 2 f f 2 f 2 1 1 . 2 
+            2 . 1 1 1 2 2 2 2 2 2 1 1 1 . 2 
+            . . . 1 1 1 1 2 2 1 1 1 1 . . .
+    """),
+    0)
+controller.move_sprite(Spaceship)
+projectile = sprites.create_projectile_from_sprite(img("""
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+"""), Spaceship, 50, 50)
+my_sprite.set_flag(SpriteFlag.StayInScreen, True)
 
-#Generate Enemies
 
-# Shoot Enemies with Projectiles
+
+
+
--- main.ts
+++ main.ts
@@ -1,41 +1,42 @@
-// Setup the game
+//  Setup the game
 info.setScore(0)
 info.setLife(3)
-scene.setBackgroundImage(img`
-    f 5 f f f f f f f f f f f f f f
-    f f f 5 f f f f f f 5 f f f f f
-    f f f f f f f 5 f f f f f 5 f f
-    f f 5 f f f f f f f f f f f f f
-    f f f f f f f f f f f f f f f f
-    f f f f f f f f f f f f 5 f f f
-    f f f 5 f f 5 5 f f f f f f f f
-    f f f f f f f f f f f f f f f f
-    f f f f f f f 5 f 5 f f f f f f
-    f f 5 f f f f f f f f f f f f f
-    f f f f f f f 5 f f f f f 5 f f
-    f f f f f f f f f f f f f f f f
-    f f f f f f f f f f f f f f f f
-    f f f f 5 f f f f f f f f f f f
-    f f 5 f f f f f 5 f f f f 5 f f
-    f f f f f f f 5 f f f f f f f f
-`)
-tiles.setTilemap(tilemap`level`)
-// Setup player
+tiles.setTilemap(tilemap`level_0`)
+//  Setup player
 let Spaceship = sprites.create(img`
-    . . . . . . . 2 2 . . . . . . .
-    . . . . . . 2 2 2 2 . . . . . .
-    . . . . . 2 2 1 1 2 2 . . . . .
-    2 . . . 2 2 1 1 1 1 2 2 . . . 2
-    2 . . . 2 1 1 1 1 1 1 2 . . . 2
-    1 . . . 2 2 1 1 1 1 2 2 . . . 1
-    1 . . f 2 2 2 2 2 2 2 2 f . . 1
-    1 . . 2 f 2 2 2 2 2 2 f 2 . . 1
-    2 . . 2 2 f 2 f f 2 f 2 2 . . 2
-    2 . 2 2 2 2 f 2 2 f 2 2 2 2 . 2
-    2 2 2 2 2 f 2 f f 2 f 2 2 2 2 2
-    2 . 2 2 2 f 2 f f 2 f 2 2 2 . 2
-    2 2 2 2 2 2 f 2 2 f 2 2 2 2 2 2
-    2 . 1 1 2 f 2 f f 2 f 2 1 1 . 2
-    2 . 1 1 1 2 2 2 2 2 2 1 1 1 . 2
-    . . . 1 1 1 1 2 2 1 1 1 1 . . .
-`)
+        . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 1 1 2 2 . . . . . 
+            2 . . . 2 2 1 1 1 1 2 2 . . . 2 
+            2 . . . 2 1 1 1 1 1 1 2 . . . 2 
+            1 . . . 2 2 1 1 1 1 2 2 . . . 1 
+            1 . . f 2 2 2 2 2 2 2 2 f . . 1 
+            1 . . 2 f 2 2 2 2 2 2 f 2 . . 1 
+            2 . . 2 2 f 2 f f 2 f 2 2 . . 2 
+            2 . 2 2 2 2 f 2 2 f 2 2 2 2 . 2 
+            2 2 2 2 2 f 2 f f 2 f 2 2 2 2 2 
+            2 . 2 2 2 f 2 f f 2 f 2 2 2 . 2 
+            2 2 2 2 2 2 f 2 2 f 2 2 2 2 2 2 
+            2 . 1 1 2 f 2 f f 2 f 2 1 1 . 2 
+            2 . 1 1 1 2 2 2 2 2 2 1 1 1 . 2 
+            . . . 1 1 1 1 2 2 1 1 1 1 . . .
+    `, 0)
+controller.moveSprite(Spaceship)
+let projectile = sprites.createProjectileFromSprite(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . 2 . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`, null, 50, 50)
