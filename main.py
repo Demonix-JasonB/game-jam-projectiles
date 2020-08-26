c1da844ca458c098462dd03e61f8ec96ad7d73a7
Demonix-JasonB <69871102+Demonix-JasonB@users.noreply.github.com>
2020-08-26 09:39:10 -0400

    Update pxt.json, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 #Setup the game
 info.set_score(0)
 info.set_life(3)
+scene.set_background_image(img("""
+    f 5 f f f f f f f f f f f f f f
+    f f f 5 f f f f f f 5 f f f f f
+    f f f f f f f 5 f f f f f 5 f f
+    f f 5 f f f f f f f f f f f f f
+    f f f f f f f f f f f f f f f f
+    f f f f f f f f f f f f 5 f f f
+    f f f 5 f f 5 5 f f f f f f f f
+    f f f f f f f f f f f f f f f f
+    f f f f f f f 5 f 5 f f f f f f
+    f f 5 f f f f f f f f f f f f f
+    f f f f f f f 5 f f f f f 5 f f
+    f f f f f f f f f f f f f f f f
+    f f f f f f f f f f f f f f f f
+    f f f f 5 f f f f f f f f f f f
+    f f 5 f f f f f 5 f f f f 5 f f
+    f f f f f f f 5 f f f f f f f f
+"""))
+tiles.set_tilemap(tilemap("""level"""))
 #Setup player
-
+Spaceship = sprites.create(img("""
+    . . . . . . . 2 2 . . . . . . .
+    . . . . . . 2 2 2 2 . . . . . .
+    . . . . . 2 2 1 1 2 2 . . . . .
+    2 . . . 2 2 1 1 1 1 2 2 . . . 2
+    2 . . . 2 1 1 1 1 1 1 2 . . . 2
+    1 . . . 2 2 1 1 1 1 2 2 . . . 1
+    1 . . f 2 2 2 2 2 2 2 2 f . . 1
+    1 . . 2 f 2 2 2 2 2 2 f 2 . . 1
+    2 . . 2 2 f 2 f f 2 f 2 2 . . 2
+    2 . 2 2 2 2 f 2 2 f 2 2 2 2 . 2
+    2 2 2 2 2 f 2 f f 2 f 2 2 2 2 2
+    2 . 2 2 2 f 2 f f 2 f 2 2 2 . 2
+    2 2 2 2 2 2 f 2 2 f 2 2 2 2 2 2
+    2 . 1 1 2 f 2 f f 2 f 2 1 1 . 2
+    2 . 1 1 1 2 2 2 2 2 2 1 1 1 . 2
+    . . . 1 1 1 1 2 2 1 1 1 1 . . .
+"""))
 #Configure Player Controls
 
 #Generate Enemies
--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
 // Setup the game
 info.setScore(0)
 info.setLife(3)
+scene.setBackgroundImage(img`
+    f 5 f f f f f f f f f f f f f f
+    f f f 5 f f f f f f 5 f f f f f
+    f f f f f f f 5 f f f f f 5 f f
+    f f 5 f f f f f f f f f f f f f
+    f f f f f f f f f f f f f f f f
+    f f f f f f f f f f f f 5 f f f
+    f f f 5 f f 5 5 f f f f f f f f
+    f f f f f f f f f f f f f f f f
+    f f f f f f f 5 f 5 f f f f f f
+    f f 5 f f f f f f f f f f f f f
+    f f f f f f f 5 f f f f f 5 f f
+    f f f f f f f f f f f f f f f f
+    f f f f f f f f f f f f f f f f
+    f f f f 5 f f f f f f f f f f f
+    f f 5 f f f f f 5 f f f f 5 f f
+    f f f f f f f 5 f f f f f f f f
+`)
+tiles.setTilemap(tilemap`level`)
+// Setup player
+let Spaceship = sprites.create(img`
+    . . . . . . . 2 2 . . . . . . .
+    . . . . . . 2 2 2 2 . . . . . .
+    . . . . . 2 2 1 1 2 2 . . . . .
+    2 . . . 2 2 1 1 1 1 2 2 . . . 2
+    2 . . . 2 1 1 1 1 1 1 2 . . . 2
+    1 . . . 2 2 1 1 1 1 2 2 . . . 1
+    1 . . f 2 2 2 2 2 2 2 2 f . . 1
+    1 . . 2 f 2 2 2 2 2 2 f 2 . . 1
+    2 . . 2 2 f 2 f f 2 f 2 2 . . 2
+    2 . 2 2 2 2 f 2 2 f 2 2 2 2 . 2
+    2 2 2 2 2 f 2 f f 2 f 2 2 2 2 2
+    2 . 2 2 2 f 2 f f 2 f 2 2 2 . 2
+    2 2 2 2 2 2 f 2 2 f 2 2 2 2 2 2
+    2 . 1 1 2 f 2 f f 2 f 2 1 1 . 2
+    2 . 1 1 1 2 2 2 2 2 2 1 1 1 . 2
+    . . . 1 1 1 1 2 2 1 1 1 1 . . .
+`)
